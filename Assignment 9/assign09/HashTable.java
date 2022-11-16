11a748e1c231af7e5d40cb3c6d3fe64fcf8e798d
Sebastian Barney <sebastianbarney1@gmail.com>
2022-11-15 18:20:06 -0700

    Fixed HashTable

--- Assignment 9/assign09/HashTable.java
+++ Assignment 9/assign09/HashTable.java
@@ -7,19 +7,27 @@ import java.util.List;
 public class HashTable<K, V> implements Map<K, V> {
 
 
-
-
-
+    //Backing Array for the HashTable
     private ArrayList<LinkedList<MapEntry<K, V>>> table;
 
+    //Initial Capacity for the HashTable Backing Array.
     private int capacity = 100;
 
+
+    //Number of Entries in the Hash Table.
     private int size;
 
 
-    double lambda = .75;
+    //Desired Lambda value for the Hash Table
+    private double lambda = 1;
 
 
+    /**
+     * Constructor for a new instance of HashTable.
+     *
+     * Creates a Backing Array of LinkedLists Containing MapEntries.
+     * Sets size to 0
+     */
     public HashTable(){
         table = new ArrayList<LinkedList<MapEntry<K, V>>>();
         for(int i = 0; i < capacity; i++)
@@ -32,170 +40,190 @@ public class HashTable<K, V> implements Map<K, V> {
 
 
     /**
-     * Removes all mappings from this map.
-     * <p>
-     * O(table length)
+     * Clears the Hash Table
      */
     @Override
     public void clear() {
         for(LinkedList<MapEntry<K,V>> list : this.table){
-        list.clear();}
+        list.clear();
+        size = 0;
+        }
     }
 
 
-    public int hashKey(K key){
+    /**
+     * Finds the necessary index for the key to go in the backing array.
+     * @param key - Key being checked.
+     * @return - Index in backing Array.
+     */
+    private int hashKey(K key){
         return key.hashCode() % this.capacity;
     }
 
 
     /**
-     * Determines whether this map contains the specified key.
-     * <p>
-     * O(1)
+     * Checks if the key is in the Hash Table.
      *
-     * @param key
-     * @return true if this map contains the key, false otherwise
+     * @param key - key being checked.
+     * @return true if HashTable has the key, false otherwise
      */
     @Override
     public boolean containsKey(K key) {
 
-        LinkedList<MapEntry<K,V>> list = table.get(hashKey(key));
+        //If the HashTable is empty the HashTable does not contain the key.
+        if(this.isEmpty()) return false;
 
+
+        //Find the Linked List with the corresponding hashKey,
+        //If the List has more than one entry loop through the linked list and return the MapEntry with the value that corresponds to the key.
+        //Otherwise, return the first item in the list.
+        //If there is no list, the item is not in the array, so it will return false.
+        LinkedList<MapEntry<K,V>> list = table.get(hashKey(key));
+        if(list.size() > 1){
         for(MapEntry<K,V> entry : list){
-        if(entry.getKey() == key){
-            return true;}}
+            if(entry.getKey().equals(key)){
+                return true;}
+            }
+        }
+        else{
+            if( list.getFirst().getKey().equals(key))
+                return true;
+        }
         return false;
     }
 
     /**
-     * Determines whether this map contains the specified value.
-     * <p>
-     * O(table length)
+     * Determines if a value is in the HashTable.
      *
-     * @param value
-     * @return true if this map contains one or more keys to the specified value,
+     * @param value - Value being checked.
+     * @return true if HashTable contains one or more keys to the specified value,
      * false otherwise
      */
     @Override
     public boolean containsValue(V value) {
 
+        //If the HashTable is Empty it does not contain the value so return false.
+        if(this.isEmpty()) return false;
+
+        //Check each list in the backing array, if the list contains a MapEntry with a value matching our parameter return true.
         for (LinkedList<MapEntry<K,V>> list : this.table){
             for(MapEntry<K,V> entry : list){
-                if(entry.getValue() == value)
+                if(entry.getValue().equals(value))
                     return true;
             }
         }
+        //False otherwise.
         return false;
     }
 
 
 
-    public boolean containsKeyValuePair(MapEntry<K,V> entry){
-
-
-        V value = entry.getValue();
-        K key = entry.getKey();
-        if(this.containsKey(key) && containsValue(value))
-            return true;
-        else return false;
-    }
     /**
-     * Returns a List view of the mappings contained in this map, where the ordering of
-     * mapping in the list is insignificant.
-     * <p>
-     * O(table length)
+     * Returns a List view of the HashTable , where the ordering is insignificant.
      *
-     * @return a List object containing all mapping (i.e., entries) in this map
+     * @return a List object containing all entries of the HashTable.
      */
     @Override
     public List<MapEntry<K, V>> entries() {
-
         ArrayList<MapEntry<K,V>> retList = new ArrayList<>();
-
         for (LinkedList<MapEntry<K,V>> list : this.table){
             retList.addAll(list);
         }
-
         return retList;
     }
 
 
+    /**
+     * Checks if an integer is prime.
+     * @param num - number being checked.
+     * @return - True if the number is prime, false otherwise.
+     */
     boolean isPrime(int num){
-
         for(int i = 2; i < num; i++){
             if(num % i == 0){
                 return false;
             }
         }
         return true;
-
     }
 
+    /**
+     * Finds an integer that is prime and at least double the size of the integer given.
+     * @param num - number being doubled and primed.
+     * @return - an integer that is at least twice the size of the input and prime.
+     */
     int doublePrime(int num){
 
+        //Find twice the input parameter.
         int dub = num*2;
-
+        //While that isn't prime increment it. Once its prime, return.
         while(!isPrime(dub)){
             dub++;
         }
         return dub;
     }
 
-
+    /**
+     * ReSizes and the HashTable and ReHashes all current Entries.
+     */
     private void reSizeReHash(){
-
+        //Creates a list of all current Entries.
         List<MapEntry<K,V>> list = this.entries();
-        int oldCap = this.capacity;
+
+        //Finds a new capacity for the backing array.
         int newCap;
         newCap = doublePrime(this.capacity);
-        clear();
-        int listsAdded = newCap - oldCap;
 
-        for(int i = 0; i < listsAdded; i++){
+        //Creates a new Backing Array of the new size. Populates with LinkedLists of Map Entries. Sets pointer.
+        table = new ArrayList<LinkedList<MapEntry<K,V>>>(newCap);
+        for(int i = 0; i < newCap; i++){
             table.add(new LinkedList<MapEntry<K,V>>());
-            this.capacity++;
         }
+
+        //Changes table capacity to new capacity. Sets Size to 0.
+        this.capacity = newCap;
         size = 0;
+        //Puts the old entries back into the array - It will automatically ReHash the table.
         for(MapEntry<K,V> entry : list){
             this.put(entry.getKey(), entry.getValue());
         }
-
-
     }
+
     /**
      * Gets the value to which the specified key is mapped.
-     * <p>
-     * O(1)
      *
-     * @param key
-     * @return the value to which the specified key is mapped, or null if this map
+     * @param key - Key that corresponds to a value.
+     * @return the value to which the specified key is mapped, or null if the HashTable
      * contains no mapping for the key
      */
     @Override
     public V get(K key) {
 
+        //if the HashTable contains the key,
         if(containsKey(key)){
+            //Get the linked list that corresponds to the key.
             LinkedList<MapEntry<K,V>> list = this.table.get(hashKey(key));
 
+            //If the list size is greater than one, get the Value that corresponds to the key.
             if(list.size() > 1){
 
                 for(MapEntry<K,V> entry : list){
-                    if(entry.getKey() == key)
+                    if(entry.getKey().equals(key))
                         return entry.getValue();
                 }
 
             }
+            //Otherwise return the first item in the list.
             else return list.getFirst().getValue();
         }
+        //If the hashTable does not contain the key, return null.
         return null;
     }
 
     /**
-     * Determines whether this map contains any mappings.
-     * <p>
-     * O(1)
+     * Checks if the HashTable has no Entries.
      *
-     * @return true if this map contains no mappings, false otherwise
+     * @return true if HashTable has entries, false otherwise.
      */
     @Override
     public boolean isEmpty() {
@@ -205,14 +233,10 @@ public class HashTable<K, V> implements Map<K, V> {
 
 
     /**
-     * Associates the specified value with the specified key in this map.
-     * (I.e., if the key already exists in this map, resets the value;
-     * otherwise adds the specified key-value pair.)
-     * <p>
-     * O(1)
      *
-     * @param key
-     * @param value
+     * Puts entry into the HashTable. Replaces Value if the Value already exists.
+     * @param key - key of entry.
+     * @param value - value of entry.
      * @return the previous value associated with key, or null if there was no
      * mapping for key
      */
@@ -221,23 +245,32 @@ public class HashTable<K, V> implements Map<K, V> {
         int hashKey = hashKey(key);
         MapEntry<K,V> entry = new MapEntry<>(key,value);
 
-        if((double)(size)/capacity >= lambda){
-            reSizeReHash();
-        }
+
 
 
             LinkedList<MapEntry<K,V>> list = this.table.get(hashKey);
+//        System.out.println(hashKey);
+//        System.out.println(Integer.toString(size) + "\t" + Integer.toString(capacity));
+
 
             for(MapEntry<K,V> kvPair : list){
-                if(kvPair.equals(entry)){
-                    return null;
+                if(kvPair.getKey().equals(key)) {
+
+
+                    if(kvPair.getValue().equals(value)){
+                    System.out.println("Key-Value Pair already exists in HashTable.");}
+                    V retVal = kvPair.getValue();
+                    kvPair.setValue(value);
+                    return retVal;
                 }
+
             }
         list.add(entry);
         size++;
-            return value;
-
-
+        if((double)(size)/capacity >= lambda){
+            reSizeReHash();
+        }
+            return null;
     }
 
     /**
@@ -287,7 +320,6 @@ public class HashTable<K, V> implements Map<K, V> {
     }
 
     public double currLambda(){
-
         return (double)(this.size) / this.capacity;
     }
 
@@ -296,5 +328,8 @@ public class HashTable<K, V> implements Map<K, V> {
     }
     public void setCapacity(int n){
         this.capacity = n;
+    }
+    public void setLambda(int n){
+        this.lambda = n;
     }
 }
--- Assignment 9/assign09/HashTableTester.java
+++ Assignment 9/assign09/HashTableTester.java
@@ -3,8 +3,10 @@ package assign09;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HashTableTester {
 
@@ -24,58 +26,191 @@ public class HashTableTester {
         for(int i = 1; i <= n; i++){
             int key = i;
             String value = Integer.toString(i);
-            assertEquals(value,testTable.put(key,value));
-            System.out.println(testTable.currLambda());
+            assertEquals(null,testTable.put(key,value));
+            assertEquals(value,testTable.get(key));
+            assertTrue(testTable.containsValue(value));
+            assertTrue(testTable.containsKey(key));
+        }
+        assertEquals(n,testTable.size());
 
+    }
 
-//            System.out.println(testTable.get(key));
-//            assertEquals(value,testTable.get(key));
+    @Test
+    void tablePutNItemsStringsStrings(){
+
+        int n = 1000;
+        HashTable<String,String> testTable = new HashTable();
+        for(int i = 1; i <= n; i++){
+            String value = Integer.toString(i);
+            String key = Integer.toString(i);
+            assertEquals(null,testTable.put(key,value));
+
+            assertEquals(value,testTable.get(key));
             assertTrue(testTable.containsValue(value));
             assertTrue(testTable.containsKey(key));
 
 //            testTable.put(key,value);
         }
 
-        System.out.println(testTable.currLambda());
-        System.out.println(testTable.getCapacity());
-        System.out.println(testTable.size());
         assertEquals(n,testTable.size());
 
     }
 
-//    @Test
-//    void tablePutNItemsStringsStrings(){
-//
-//        int n = 101;
-//        HashTable<String,String> testTable = new HashTable();
-//        for(int i = 1; i <= n; i++){
-//            String value = Integer.toString(i);
-//            String key = Integer.toString(i);
-//            assertEquals(value,testTable.put(key,value));
-//            assertEquals(value,testTable.get(key));
-//            assertTrue(testTable.containsValue(value));
-//            assertTrue(testTable.containsKey(key));
-//
-////            testTable.put(key,value);
-//        }
-//
-//        assertEquals(n,testTable.size());
-//
-//    }
-
 
 
     @Test
-    void doStuff() {
+    void worstCaseFixesItself() {
         HashTable<Integer, String> testTable = new HashTable();
 
-        assertEquals("1", testTable.put(1, "1"));
-        assertEquals("2", testTable.put(101, "2"));
-        assertEquals("3", testTable.put(201, "3"));
-        System.out.println(testTable.get(1));
-        System.out.println(testTable.get(101));
-        System.out.println(testTable.get(201));
+        int n = 99;
+        for(int i = 0; i < n; i++){
+        int key = 1 + 100*i;
+        String value = Integer.toString(i);
+            assertEquals(null,testTable.put(key,value));
+            assertTrue(testTable.containsKey(key));
+            assertTrue(testTable.containsValue(value));
+            assertEquals(testTable.getCapacity(),100);
+        }
+        testTable.put((100*100+1),"100");
+        assertNotEquals(100, testTable.getCapacity());
+
+    }
+
+    @Test
+    void sizeChanges(){
+        HashTable<Integer, String> testTable = new HashTable();
+
+        int n = 1000;
+        for(int i = 1; i <= n; i++){
+            int key = i;
+            String value = Integer.toString(i);
+            assertEquals(null,testTable.put(key,value));
+            assertTrue(testTable.containsKey(key));
+            assertTrue(testTable.containsValue(value));
+            assertEquals(i,testTable.size());
+        }
+
+        assertEquals(testTable.size(),n);
+        testTable.remove(3);
+        assertNotEquals(testTable.size(),n);
 
 
     }
+
+    @Test
+    void entriesMethod(){
+        int n = 1000;
+        HashTable<Integer,String> testTable = new HashTable();
+        ArrayList<MapEntry<Integer,String>> testList = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            int key = i;
+            String value = Integer.toString(i);
+            assertEquals(null,testTable.put(key,value));
+            assertEquals(value,testTable.get(key));
+            assertTrue(testTable.containsValue(value));
+            assertTrue(testTable.containsKey(key));
+            testList.add(new MapEntry<>(key,value));
+        }
+        List<MapEntry<Integer,String>> list = testTable.entries();
+
+        int size = list.size();
+        assertEquals(size,testList.size());
+        for(int i = 0; i < size; i++){
+            assertEquals(list.get(i),testList.get(i));
+        }
+    }
+    @Test
+    void isEmptyMethod(){
+        int n = 1000;
+        HashTable<Integer,String> testTable = new HashTable();
+
+        assertTrue(testTable.isEmpty());
+        for(int i = 1; i <= n; i++){
+            int key = i;
+            String value = Integer.toString(i);
+            assertEquals(null,testTable.put(key,value));
+            assertEquals(value,testTable.get(key));
+            assertTrue(testTable.containsValue(value));
+            assertTrue(testTable.containsKey(key));
+        }
+        assertFalse(testTable.isEmpty());
+        assertFalse(testTable.entries().isEmpty());
+
+
+    }
+    @Test
+    void clearMethod(){
+        int n = 1000;
+        HashTable<Integer,String> testTable = new HashTable();
+
+        assertTrue(testTable.isEmpty());
+        for(int i = 1; i <= n; i++){
+            int key = i;
+            String value = Integer.toString(i);
+            assertEquals(null,testTable.put(key,value));
+            assertEquals(value,testTable.get(key));
+            assertTrue(testTable.containsValue(value));
+            assertTrue(testTable.containsKey(key));
+        }
+        assertFalse(testTable.isEmpty());
+        testTable.clear();
+        assertTrue(testTable.isEmpty());
+        assertTrue(testTable.entries().isEmpty());
+
+    }
+    @Test
+    void backingArrGrows() {
+        HashTable<Integer, String> testTable = new HashTable();
+
+        int n = 99;
+        for(int i = 0; i < n; i++){
+            int key = i;
+            String value = Integer.toString(i);
+            assertEquals(null,testTable.put(key,value));
+            assertTrue(testTable.containsKey(key));
+            assertTrue(testTable.containsValue(value));
+            assertEquals(testTable.getCapacity(),100);
+            assertEquals(i+1,testTable.size());
+        }
+        testTable.put(100,"100");
+        assertEquals(100,testTable.size());
+        assertNotEquals(100, testTable.getCapacity());
+
+    }
+
+    @Test
+    void removeItem(){
+
+        int n = 1000;
+        HashTable<Integer,String> testTable = new HashTable();
+        for(int i = 1; i <= n; i++) {
+            int key = i;
+            String value = Integer.toString(i);
+            assertEquals(null, testTable.put(key, value));
+            assertTrue(testTable.containsValue(value));
+            assertTrue(testTable.containsKey(key));
+            testTable.remove(key);
+            assertFalse(testTable.containsValue(value));
+            assertFalse(testTable.containsKey(key));
+        }
+        assertEquals(testTable.size(), 0);
+
+
+    }
+
+
+    @Test
+    void replaceItem(){
+        HashTable<Integer,String> testTable = new HashTable();
+        for(int i = 0; i < 10; i++){
+        testTable.put(i,Integer.toString(i));
+        assertEquals(Integer.toString(i),testTable.get(i));
+        }
+        String val = testTable.get(3);
+        assertEquals(testTable.get(3),val);
+        assertEquals(testTable.put(3,"100"),val);
+        assertNotEquals(testTable.get(3),val);
+    }
+
+
 }
