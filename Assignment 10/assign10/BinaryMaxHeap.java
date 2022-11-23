f9f0f80f5197926009456ba86ce10a4028fab565
Sebastian Barney <sebastianbarney1@gmail.com>
2022-11-23 01:44:59 -0700

    Tester File and Changes to heap methods

--- Assignment 10/assign10/BinaryMaxHeap.java
+++ Assignment 10/assign10/BinaryMaxHeap.java
@@ -3,121 +3,188 @@ package assign10;
 import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
-
+/**
+ * Binary MaxHeap data structure that uses a Generic Data Type.
+ * Implements Priority Queue interface.
+ * {@code @Authors} Sebastian Barney and Amelia Neilson
+ * @param <E> - Generic type E for Element
+ */
 public class BinaryMaxHeap<E> implements PriorityQueue<E>{
+
+
+	/**
+	 * Creates a backing array variable.
+	 * Internally,
+	 * keeps track of the Index within the backing array needs to be called for the smallest element added.
+	 * keeps track of the "size" of the heap which is how many elements have been added to it.
+	 * keeps track of a comparator, and if it is needed.
+	 * Has 2 private variables to initialize size and growth factor for the array.
+	 */
+
+	private E[] backingArray;
 	private int currentAddInd;
-	private int initSize = 10;
-	private int growFactor = 2;
 	private int size;
 	private Comparator<? super E> comparator;
-
 	private boolean isComparator = false;
-	
-	private E[] backingArray;
-	
+	private int initSize = 10;
+	private int growFactor = 2;
+
+
+	/**
+	 * Empty Constructor for the class. Creates an empty BinaryMaxHeap.
+	 * Initializes the empty array to the initial size.
+	 * Initializes the size variable and current Add Index variables;
+	 */
 	@SuppressWarnings("unchecked")
 	public BinaryMaxHeap() {
 	backingArray = (E[]) new Object[initSize];
 	size = 0;
 	currentAddInd = 0;
 	}
-	
+
+	/**
+	 * Constructor for class with a comparator argument.
+	 * Creates an empty binary heap that uses a comparator.
+	 * Initializes variables and backing array. Changes method to use comparator.
+	 * @param comp - comparator being used.
+	 */
+	@SuppressWarnings("unchecked")
 	public BinaryMaxHeap(Comparator<? super E> comp) {
 		isComparator = true;
 		comparator = comp;
-
+		backingArray = (E[]) new Object[initSize];
+		size = 0;
+		currentAddInd = 0;
 	}
-	
+
+	/**
+	 * Constructor for class with a list to add to the heap.
+	 * Creates a binary max heap with the list being the data the heap contains.
+	 * Initializes variables and arrays then adds everything in the list to the backingArray.
+	 * After everything has been added to the array, it gets heapified.
+	 * @param dataList - list of items to be added to the heap.
+	 */
+	@SuppressWarnings("unchecked")
 	public BinaryMaxHeap(List<? extends E> dataList) {
 		size = 0;
 		currentAddInd = 0;
 		backingArray = (E[]) new Object[dataList.size()];
 		buildHeap(dataList);
 	}
-	
-	public BinaryMaxHeap(List<? extends E> datalist, Comparator<? super E> comp) {
+
+	/**
+	 * Constructor that creates a binary heap with both a comparator and a list.
+	 * Essentially a combination of the other two constructors.
+	 * @param dataList - list of items to be added to the heap.
+	 * @param comp - comparator being used.
+	 */
+	@SuppressWarnings("unchecked")
+	public BinaryMaxHeap(List<? extends E> dataList, Comparator<? super E> comp) {
 		isComparator = true;
 		comparator = comp;
 		size = 0;
 		currentAddInd = 0;
-		backingArray = (E[]) new Object[datalist.size()];
-		buildHeap(datalist);
+		backingArray = (E[]) new Object[dataList.size()];
+		buildHeap(dataList);
 	}
 
+	/**
+	 * Adds an Item to the heap.
+	 * @param item - item to be added.
+	 */
 	@Override
 	public void add(E item) {
+		// if the backing array is too small then make it bigger.
 		if (currentAddInd >= backingArray.length) {
 			growArray();
 		}
+		//add the item to the index added.
+		//Increment the size and the index.
+		//percolate the item in the heap structure.
 		backingArray[currentAddInd] = item;
 		size++;
 		currentAddInd++;
-		percolateUp(item, currentAddInd);
-
+		percolateUp(item, currentAddInd-1);
 	}
 
 
-
+	/**
+	 * peeks the max item in the heap.
+	 * @return - the item.
+	 * @throws NoSuchElementException - if the heap is empty there is no max so throw.
+	 */
 	@Override
 	public E peek() throws NoSuchElementException {
-		E data = this.backingArray[0];
-		return data;
+		if(this.isEmpty()){throw new NoSuchElementException();}
+		return this.backingArray[0];
 	}
 
+	/**
+	 * Extracts the max item from the heap.
+	 * @return - the item.
+	 * @throws NoSuchElementException - if the heap is empty there is no max so throw.
+	 */
 	@Override
 	public E extractMax() throws NoSuchElementException {
 		E max = peek();
 		E data = backingArray[currentAddInd - 1];
 		backingArray[0] = data;
-		backingArray[currentAddInd - 1] = null;
-		percolateDown(data,0);
+
+		backingArray[currentAddInd] = null;
+		percolateDown(data);
+
 		size--;
 		currentAddInd--;
 		return max;
+
 	}
 
+	/**
+	 * Gets the size of the heap, which is the number of elements in the heap.
+	 * @return - size.
+	 */
 	@Override
 	public int size() {
-		int checkSize = 0;
-		for(E element : this.backingArray){
-			if(!element.equals(null)){
-				checkSize++;
-			}
-		}
-		if(size != checkSize){
-			size = checkSize;
-		}
 		return size;
 	}
 
+	/**
+	 * Checks if the heap is empty.
+	 * @return true if the heap is empty, false otherwise.
+	 */
 	@Override
 	public boolean isEmpty() {
-		for(E element : this.backingArray){
-			if(!element.equals(null)){
-				return false;
-			}
-		}
-		return true;
+		return size == 0;
 	}
 
-	@Override
+	/**
+	 * Clears the heap by reinitializing size, index, and backing array variables.
+	 */
+	@Override@SuppressWarnings("unchecked")
 	public void clear() {
 		size = 0;
 		currentAddInd = 0;
 		backingArray = (E[])new Object[initSize];
 	}
 
+	/**
+	 * turns the heap into an array.
+	 * @return object array containing heap data.
+	 */
 	@Override
 	public Object[] toArray() {
 
 		Object[] retArray = new Object[size];
-		for(int index = 0; index < size; index++){
-			retArray[index] = this.backingArray[index];
-		}
+		System.arraycopy(this.backingArray, 0, retArray, 0, size);
 		return retArray;
 	}
-	
+
+	/**
+	 * builds a heap from a list.
+	 * @param list - list to create heap from.
+	 */
 	private void buildHeap(List<? extends E>list) {
 		while(backingArray.length < list.size()){
 			growArray();
@@ -127,16 +194,19 @@ public class BinaryMaxHeap<E> implements PriorityQueue<E>{
 			backingArray[index] = list.get(index);
 		}
 		for(int i = 0; i <= lastCheckIndex; i++){
-			percolateDown(backingArray[i],i);
+			percolateDown(backingArray[i]);
 		}
 	}
-	
-	private void percolateUp(E element, int index) {
 
+	/**
+	 * Percolates an element up the heap.
+	 * @param element - element to be percolated.
+	 * @param index - index of the element.
+	 */
+	private void percolateUp(E element, int index) {
 		int prevInd = index;
 		index = (index - 1)/2;
-
-		while (innerCompare(element,backingArray[index]) >= 0){
+		while (innerCompare(element,backingArray[index]) >= 0 && prevInd != 0){
 			E data1 = backingArray[prevInd];
 			E data2 = backingArray[index];
 			backingArray[index] = data1;
@@ -145,34 +215,61 @@ public class BinaryMaxHeap<E> implements PriorityQueue<E>{
 			index = (index - 1)/2;
 		}
 	}
-	
-	private void percolateDown(E element, int index) {
-		index = (index*2)+1;
-		while (innerCompare(element,backingArray[index]) < 0){
 
-			int leftChild = index;
-			int rightChild = index+1;
-			if(backingArray[rightChild].equals(null)){
-				if(innerCompare(element,backingArray[leftChild]) <= 0);
-				backingArray[index] = element;
-			}
-			else{
-				int greaterChild;
-				if(innerCompare(backingArray[leftChild],backingArray[rightChild]) > 0){
-					 backingArray[leftChild] = element;
+	/**
+	 * Percolates an element down the heap.
+	 * @param element - element to be percolated.
+	 */
+	private void percolateDown(E element) {
+		for(int index = 1; index < backingArray.length; index+=2){
 
+			
+			int leftChildIndex = index;
+			int rightChildIndex = index + 1;
+			E leftChild = backingArray[leftChildIndex];
+			E rightChild = backingArray[rightChildIndex];
+
+			if((Objects.isNull(leftChild) && Objects.isNull(rightChild)) || (Objects.isNull(element))){
+				break;
+			}
+			if(Objects.isNull(leftChild) || Objects.isNull(rightChild)){
+				if(!Objects.isNull(leftChild)){
+					int compVal = innerCompare(element,leftChild);
+					if(compVal == 1){
+						backingArray[leftChildIndex] = element;
+					} else if (compVal == 0) {
+						break;
+					}
+					else {
+						continue;
+					}
 				}
-				else{
-					backingArray[rightChild] = element;
+				if(!Objects.isNull(rightChild)){
+					int compVal = innerCompare(element,rightChild);
+					if(compVal == 1){
+						backingArray[rightChildIndex] = element;
+					} else if (compVal == 0) {
+						break;
+					}
+					else {
+						continue;
+					}
 				}
 			}
-			index = (index*2)+1;
 		}
 	}
 
+	/**
+	 * Compares two elements.
+	 * @param elementLeft - object for comparing.
+	 * @param elementRight - object to compare.
+	 * @return 1 if the left element is larger than the right. 0 if they are equal. -1 if it is less than or null.
+	 */
+	@SuppressWarnings("unchecked")
 	private int innerCompare(E elementLeft, E elementRight) {
-
-
+		if(Objects.isNull(elementLeft) || Objects.isNull(elementRight)){
+			return -1;
+		}
 		if(isComparator){
 			return comparator.compare(elementLeft,elementRight);
 		}
@@ -181,15 +278,61 @@ public class BinaryMaxHeap<E> implements PriorityQueue<E>{
 		}
 	}
 
-	
+	/**
+	 * Grows the backing Array by some growth factor, kept track of internally.
+	 */
+	@SuppressWarnings("unchecked")
 	private void growArray() {
-
+		//Makes an array that is larger, then copies the data over.
 		int backArrLen = this.backingArray.length;
 		E[] tempArray = (E[]) new Object[backArrLen*growFactor];
-		for(int i = 0; i < backArrLen;i++) {
-			tempArray[i] = this.backingArray[i];
-		}
+		System.arraycopy(this.backingArray, 0, tempArray, 0, backArrLen);
 		this.backingArray = tempArray;
+	}
+	/*
+	   ===========================================
+	   =             Testing Methods             =
+	   ===========================================
+	 */
+
+	/**
+	 * Gets the backing array.
+	 * @return the backing array of the heap.
+	 */
+	public E[] getBackingArray() {
+		return backingArray;
+	}
+
+	/**
+	 * Sets the initial size variable.
+	 * @param n - what it gets set to.
+	 */
+	public void setInitSize(int n){
+		initSize = n;
+	}
+
+	/**
+	 * Gets the initial size
+	 * @return initial size.
+	 */
+	public int getInitSize(){
+		return initSize;
+	}
+
+	/**
+	 * Sets growth factor.
+	 * @param n - what it gets set to.
+	 */
+	public void setGrowthFactor(int n){
+		growFactor = n;
+	}
+
+	/**
+	 * gets the growth factor.
+	 * @return - the growth factor.
+	 */
+	public int getGrowthFactor(){
+		return growFactor;
 	}
 
 }
--- Assignment 10/assign10/BinaryMaxHeapTester.java
+++ Assignment 10/assign10/BinaryMaxHeapTester.java
@@ -1,4 +1,183 @@
 package assign10;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * BinaryMaxHeap Tests
+ * By @Authors Sebastian Barney and Amelia Neilson.
+ */
 public class BinaryMaxHeapTester {
+
+    @Test
+    void addTestingInOrder() {
+        BinaryMaxHeap<Integer> tester = new BinaryMaxHeap<Integer>();
+        for (int i = 10; i > 0; i--) {
+            tester.add(i);
+        }
+        Object[] results = tester.getBackingArray();
+        for (int i = 0; i < 10; i++) {
+            assertEquals(10 - i, (Integer) results[i]);
+        }
+    }
+
+    @Test
+    void addTestingOutOfOrderPercolate() {
+        BinaryMaxHeap<Integer> tester = new BinaryMaxHeap<Integer>();
+        tester.add(1);
+        tester.add(8);
+        tester.add(6);
+        tester.add(2);
+        tester.add(3);
+        tester.add(5);
+        tester.add(9);
+        Object[] results = tester.getBackingArray();
+        assertEquals(9, results[0]);
+        assertEquals(3, results[1]);
+        assertEquals(8, results[2]);
+        assertEquals(1, results[3]);
+        assertEquals(2, results[4]);
+        assertEquals(5, results[5]);
+        assertEquals(6, results[6]);
+    }
+
+    @Test
+    void resizingSizeTests() {
+        BinaryMaxHeap<Integer> tester = new BinaryMaxHeap<Integer>();
+        Object[] results = tester.getBackingArray();
+        //Checks for correct size and backing array in beginning
+        assertEquals(10, results.length);
+        assertEquals(0, tester.size());
+        //Checks that size appropriately grows
+        for (int i = 0; i < 10; i++) {
+            tester.add(i);
+            assertEquals(i + 1, tester.size());
+            results = tester.getBackingArray();
+            assertEquals(10, results.length);
+        }
+        //Checks that size appropriately grows as does the backing array
+        for (int i = 0; i < 10; i++) {
+            tester.add(i);
+            assertEquals(i + 11, tester.size());
+            results = tester.getBackingArray();
+            assertEquals(20, results.length);
+        }
+        //Last check that backing array repetitively grows as expected
+        tester.add(21);
+        assertEquals(21, tester.size());
+        results = tester.getBackingArray();
+        assertEquals(40, results.length);
+//Checks that removal affects size appropriately
+        for (int i = 20; i > 10; i--) {
+            tester.extractMax();
+            assertEquals(i, tester.size());
+        }
+        //Checks that size works appropriately after/alongside removal
+        for (int i = 0; i < 5; i++) {
+            assertEquals(11, tester.size());
+            tester.add(i);
+            assertEquals(12, tester.size());
+            tester.extractMax();
+            assertEquals(11, tester.size());
+        }
+//        Checks that size functions all the way to complete removal
+        for (int i = 11; i > 0; i--) {
+            assertEquals(i, tester.size());
+            tester.extractMax();
+
+
+        }
+    }
+
+    @Test
+    void clearAndIsEmpty() {
+        int n = 100;
+        BinaryMaxHeap<Integer> testHeap = new BinaryMaxHeap<>();
+
+        assertTrue(testHeap.isEmpty());
+        assertEquals(testHeap.size(), 0);
+        for (int i = 0; i < n; i++) {
+            testHeap.add(i);
+            assertFalse(testHeap.isEmpty());
+            assertNotEquals(testHeap.size(), 0);
+        }
+        assertFalse(testHeap.isEmpty());
+        assertNotEquals(testHeap.size(), 0);
+        testHeap.clear();
+        assertTrue(testHeap.isEmpty());
+        assertEquals(testHeap.size(), 0);
+
+    }
+
+
+    @Test
+    void extractMaxOutOfOrder() {
+        BinaryMaxHeap<Integer> tester = new BinaryMaxHeap<Integer>();
+        tester.add(1);
+        tester.add(8);
+        tester.add(6);
+        tester.add(2);
+        tester.add(3);
+        tester.add(5);
+        tester.add(9);
+        System.out.println(Arrays.toString(tester.toArray()));
+        assertEquals(9, tester.extractMax());
+        System.out.println(Arrays.toString(tester.toArray()));
+
+        assertEquals(8, tester.extractMax());
+        System.out.println(Arrays.toString(tester.toArray()));
+
+        assertEquals(6, tester.extractMax());
+        System.out.println(Arrays.toString(tester.toArray()));
+
+        assertEquals(5, tester.extractMax());
+        assertEquals(3, tester.extractMax());
+        assertEquals(2, tester.extractMax());
+        assertEquals(1, tester.extractMax());
+    }
+
+    @Test
+    void toArrayInOrderTests() {
+        BinaryMaxHeap<Integer> tester = new BinaryMaxHeap<Integer>();
+        for (int i = 10; i > 0; i--) {
+            tester.add(i);
+        }
+        Object[] checker = new Object[10];
+        for (int i = 10; i > 0; i--) {
+            checker[10 - i] = i;
+        }
+        Object[] results = tester.toArray();
+        for (int i = 0; i < 10; i++) {
+            assertEquals(checker[i], results[i]);
+        }
+    }
+
+    @Test
+    void toArrayOutOfOrderTests() {
+        BinaryMaxHeap<Integer> tester = new BinaryMaxHeap<Integer>();
+        tester.add(1);
+        tester.add(8);
+        tester.add(6);
+        tester.add(2);
+        tester.add(3);
+        tester.add(5);
+        tester.add(9);
+        Object[] checker = new Object[7];
+        checker[0] = 9;
+        checker[1] = 3;
+        checker[2] = 8;
+        checker[3] = 1;
+        checker[4] = 2;
+        checker[5] = 5;
+        checker[6] = 6;
+        Object[] results = tester.toArray();
+        for (int i = 0; i < 7; i++) {
+            assertEquals(checker[i], results[i]);
+        }
+
+
+    }
 }
